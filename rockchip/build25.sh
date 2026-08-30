eaeea4159b4da7d192e37b50b0a236923b2fd8b8
Crazy-Ice <107289800+Crazy-Ice@users.noreply.github.com>
2026-08-30 18:10:22 +0800

    Remove unnecessary packages from build script
    Removed several packages related to OpenClash, WOL, and Passwall from the build script.

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -55,11 +55,6 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-app-wol"
-PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
-PACKAGES="$PACKAGES luci-app-passwall"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
